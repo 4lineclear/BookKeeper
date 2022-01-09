f53d6859d985c42f88f130d944886e90774cdade
4lineclear <83737053+4lineclear@users.noreply.github.com>
2022-01-09 00:36:06 -0500

    Created TablePanelNode Is custom control inheriting from TableLayoutPanel, intended to make switching between panels easy Will implement into MainForm at later date

---
+++ View/TablePanelNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookKeeper.View
+{
+    public class TablePanelNode : TableLayoutPanel
+    {
+        [Category("Node Attributes")]
+        public NodeType Type { get; set; }
+
+        private TablePanelNode _childPanel;
+        
+        [Category("Node Attributes")]
+        public TablePanelNode ChildPanel
+        {
+            get{
+                return Type == NodeType.Leaf ? this : _childPanel;
+            }
+            set{
+                if (Type != NodeType.Leaf)
+                    _childPanel = value;
+            }
+        }
+
+        private TablePanelNode _parentPanel;
+        
+        [Category("Node Attributes")]
+        public TablePanelNode ParentPanel { 
+            get { 
+                return Type == NodeType.Root ? this : _parentPanel; 
+            } 
+            set {
+                if (Type != NodeType.Root)
+                    _parentPanel = value;
+            } 
+        }
+        
+        public enum NodeType
+        {
+            Root,
+            Parent,
+            Leaf
+        }
+    }
+}
